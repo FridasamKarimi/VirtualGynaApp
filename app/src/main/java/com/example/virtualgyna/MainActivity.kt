7a40a1736c4d5ec6c93fb28d379492fb720e43c5
jeff <geoffreyerastus956@gmail.com>
2024-03-22 00:27:39 -0400

    splash screen

--- app/src/main/java/com/example/virtualgyna/MainActivity.kt
+++ app/src/main/java/com/example/virtualgyna/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.virtualgyna
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.virtualgyna.auth.Login
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
+
+
 }
+
