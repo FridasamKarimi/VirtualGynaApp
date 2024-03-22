8b1c70209c401136261ffb5aa0520ef40958a04e
jeff <geoffreyerastus956@gmail.com>
2024-03-22 01:23:58 -0400

    auth logic basic

--- app/src/main/java/com/example/virtualgyna/auth/Register.kt
+++ app/src/main/java/com/example/virtualgyna/auth/Register.kt
@@ -1,11 +1,27 @@
 package com.example.virtualgyna.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.virtualgyna.databinding.ActivityRegisterBinding
+import com.example.virtualgyna.screens.Home
 
 class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+            finish()
+        }
+
+        binding.tvLogin.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
     }
 }
